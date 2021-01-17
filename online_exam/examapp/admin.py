150a4713301d8b184dad274f14b9fc773352e020
lympassion <455283303@qq.com>
2021-01-17 15:43:33 +0800

    登录完成

--- online_exam/examapp/admin.py
+++ online_exam/examapp/admin.py
@@ -13,7 +13,7 @@ class UserAdmin(UserAdmin):
 
     # 自定义管理界面
     is_teacher.short_description = '身份'
-    list_display = ['uid', 'username', 'real_name', 'class_name', is_teacher]
+    list_display = ['username', 'real_name', 'class_name', is_teacher]
     list_filter = ['class_name', 'is_teacher']
     search_fields = ['class_name', 'username', 'real_name']
     list_per_page = 20
--- online_exam/examapp/urls.py
+++ online_exam/examapp/urls.py
@@ -5,10 +5,5 @@ from . import views
 # 增加域名空间
 app_name = 'examapp'
 urlpatterns = [
-    # path('login/', views.user_login, name='login'),
     path('login/', views.user_login, name='login'),
-    #     # ex: /polls/5/
-#     path('<int:question_id>/', views.detail, name='detail'),
-#     path('<int:question_id>/results/', views.results, name='results'),
-#     path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
